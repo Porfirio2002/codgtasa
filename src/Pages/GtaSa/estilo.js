dc585b90a2427ea4c307e86fc35c79d6f82167a0
Porfirio2002 <pedrohenriquedasilva014@gmail.com>
2020-12-27 03:40:37 -0300

    atua

---
+++ src/Pages/GtaSa/estilo.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import {StyleSheet} from 'react-native';
+
+const estilo = StyleSheet.create({
+    container: {
+        backgroundColor: "black",
+        alignItems: "center"
+       
+               
+    },
+
+    texto: {
+        color: "white",
+        margin: 5
+    },
+    videinfi: {
+        color: "red",
+        margin: 5
+    },
+    titulo: {
+        textAlign: "center",
+        fontWeight: "bold",
+        color: "white",
+        margin: 15
+    },
+    contato: {
+        textAlign: "center",
+        fontWeight: "bold",
+        color: "white",
+        margin: 20,
+        fontSize: 25,
+        color: "red"
+    },
+    maps: {
+        width: 350,
+        height: 242,
+        alignItems: "center"
+    },
+    maps2: {
+        width: 350,
+        height: 500,
+        alignItems: "center"
+    },
+    maps3: {
+        width: 365,
+        height: 500,
+        alignItems: "center"
+    },
+    maps4: {
+        width: 350,
+        height: 300,
+        alignItems: "center"
+    },
+    
+    
+})
+
+export default estilo;
---
+++ src/Pages/GtaSa/home.js
@@ -0,0 +1,160 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { ScrollView, StyleSheet, Text, View, Image } from 'react-native';
+import estilo from './estilo';
+
+const Home = () => {
+    return (
+        <ScrollView>
+        <View style={estilo.container}>
+      <View>
+            <Text style={estilo.titulo}>CODIGOS GTA SAN ANDREAS PS2</Text>
+         <Text style={estilo.texto}>  $250,000, Saúde e Armadura completas: R1, R2, L1 , X, Esquerda , Baixo, Direita, Cima, Esquerda, Baixo, Direita, Cima </Text>
+         <Text style={estilo.texto}>  Condutores agressivos: Direita, R2, Cima, Cima, R2, Círculo, Quadrado, R2, L1, Direita, Baixo, L1 </Text>
+         <Text style={estilo.texto}>  Trânsito Agressivo: R2, Círculo, R1, L2, Esquerda R1, L1, R2, L2 </Text>
+         <Text style={estilo.texto}>  Todos os carros têm nitro: Esquerda, Triângulo, R1, L1, Cima, Quadrado, Triângulo, Baixo, Círculo, L2, L1, L1 </Text>
+         <Text style={estilo.texto}>  Todos os NPC são Elvis: L1, Círculo, Triângulo, L1, L1, Quadrado, L2, Cima, Baixo, Esquerda </Text>
+         <Text style={estilo.texto}>  Todos os táxis têm nitro + saltam para cima quando primes L3: Cima, X, Triângulo, X, Triângulo, X, Quadrado, R2, Direita </Text>
+         <Text style={estilo.texto}>  Trânsito com carros de lixo: L2, Direita, L1, Cima, X, L1, L2, R2, R1, L1, L1, L1 </Text>
+         <Text style={estilo.texto}>  Todos os semáforos ficam verdes: Direita, R1, Cima, L2, L2, Esquerda, R1, L1, R1, R1 </Text>
+         <Text style={estilo.texto}>  Todos os veículos ficam invisíveis menos as motos: Triângulo, L1, Triângulo, R2, Quadrado, L1, L1 </Text>
+         <Text style={estilo.texto}>  Sempre Meia-noite (O tempo fica em 00:00): Quadrado, L1, R1, Direita, X, Cima, L1, Esquerda, Esquerda </Text>
+         <Text style={estilo.texto}>  Modo Adrenalina: X, X, Quadrado, R1, L1, X, Baixo, Esquerda, X </Text>
+         <Text style={estilo.texto}>  Explodir Veículos: L1, L2, L2, Cima, Baixo, Baixo, Cima, R1, R2, R2 </Text>
+         <Text style={estilo.texto}>  Atrair prostitutas com brinquedos sexuais: Quadrado, Direita, Quadrado, Quadrado, L2, X, Triângulo, X, Triângulo </Text>
+         <Text style={estilo.texto}>  Trânsito Negro: Círculo, L2, Cima, R1, Esquerda, X, R1, L1, Esquerda, Círculo </Text>
+         <Text style={estilo.texto}>  Carros flutuam quando batidos: Quadrado, R2, Baixo, Baixo, Esquerda, Baixo, Esquerda, Esquerda, L2, X </Text>
+         <Text style={estilo.texto}>  Carros voam: Quadrado, Baixo, L2, Cima, L1, Círculo, Cima, X, Esquerda </Text>
+         <Text style={estilo.texto}>  Carros em água: Direita, R2, Círculo, R1, L2, Quadrado, R1, R2 </Text>
+         <Text style={estilo.texto}>  Modo caos: L2, Direita, L1, Triângulo, Direita, Direita, R1, L1, Direita, L1, L1, L1 </Text>
+         <Text style={estilo.texto}>  Área com temática de palhaços: Triângulo, Triângulo, L1, Quadrado, Quadrado, Círculo, Quadrado, Baixo, Círculo </Text>
+         <Text style={estilo.texto}>  Cometer suicídio: Direita, L2, Baixo, R1, Esquerda, Esquerda, R1, L1, L2, L1 </Text>
+         <Text style={estilo.texto}>  Destruir carros: R2, L2, R1, L1, L2, R2, Quadrado, Triângulo, Círculo, Triângulo, L2, L1 </Text>
+         <Text style={estilo.texto}>  Área com temática de Bikinis: Cima, Cima, Baixo, Baixo, Quadrado, Círculo, L1, R1, Triângulo, Baixo </Text>
+         <Text style={estilo.texto}>  Relógio mais rápido: Círculo, Círculo, L1, Quadrado, L1, Quadrado, Quadrado, Quadrado, L1, Triângulo, Círculo, Triângulo </Text>
+         <Text style={estilo.texto}>  Gameplay mais rápido: Triângulo, Cima, Direita, Baixo, L2, L1, Quadrado </Text>
+         <Text style={estilo.texto}>  Barcos voadores: R2, Círculo, Cima, L1, Direita, R1, Direita, Cima, Quadrado, Triângulo </Text>
+         <Text style={estilo.texto}>  Nevoeiro: R2, X, L1, L1, L2, L2, L2, X </Text>
+         <Text style={estilo.texto}>  Mira enquanto dirige: Cima, Cima, Quadrado, L2, Direita, X, R1, Baixo, R2, Círculo </Text>
+         <Text style={estilo.texto}>  Membros de gangs fazem spawn mais depressa: Esquerda, Direita, Direita, Direita, Esquerda, X, Baixo, Cima, Quadrado, Direita </Text>
+         <Text style={estilo.texto}>  Nível de procura em seis estrelas: Círculo, Direita, Círculo, Direita, Esquerda, Quadrado, X Baixo </Text>
+         <Text style={estilo.texto}>  Hitman em todas as armas: Baixo, Quadrado, X, Esquerda, R1, R2, Esquerda, Baixo, Baixo, L1, L1, L1 </Text>
+         <Text style={estilo.texto}>  Munição infinita: L1, R1, Quadrado, R1, Esquerda, R2, R1, Esquerda, Quadrado, Baixo, L1, L1 </Text>
+         <Text style={estilo.videinfi}> VIDA INFINITA: Baixo, X, Direita, Esquerda, Direita, R1, Direita, Baixo, Cima, Triângulo </Text>
+         <Text style={estilo.texto}>  Capacidade pulmonar infinita: Baixo, Esquerda, L1, Baixo, Baixo, R2, Baixo, L2, Baixo </Text>
+         <Text style={estilo.texto}>  Manuseio insano: Triângulo, R1, R1, Esquerda, R1, L1, R2, L1 </Text>
+         <Text style={estilo.texto}>  mochila a jato: Esquerda, Direita, L1, L2, R1, R2, Cima, Baixo, Esquerda, Direita </Text>
+         <Text style={estilo.texto}>  Super Pulo: Cima, Cima, Triângulo, Triângulo, Cima, Cima, Esquerda, Direita, Quadrado, R2, R2 </Text>
+         <Text style={estilo.texto}>  Saltos altos em bicicletas: Triângulo, Quadrado, Círculo, Círculo, Quadrado, Círculo, Círculo, L1, L2, L2, R1, R2. </Text>
+         <Text style={estilo.texto}>  Bloquear nível de procurado na classificação de estrelas actual: Círculo, Direita, Círculo, Direita, Esquerda, Quadrado, Triângulo, Cima </Text>
+         <Text style={estilo.texto}>  Baixar o nível de procurado: R1, R1, Círculo, R2, Cima, Baixo, Cima, Baixo, Cima, Baixo </Text>
+         <Text style={estilo.texto}>  Colocar as estatísticas de todos os veículos no máximo (Condução, bicicleta voadora, ciclista): Quadrado, L2, X, R1, L2, L2, Esquerda, R1, Direita, L1, L1, L1 </Text>
+         <Text style={estilo.texto}>  Gordola: Triângulo, Cima ,Cima, Esquerda, Direita, Quadrado, Círculo, Baixo </Text>
+         <Text style={estilo.texto}>  Trincado: Triângulo,Cima,Cima,Esquerda,Direita,Quadrado,Círculo,Esquerda </Text>
+         <Text style={estilo.texto}>  Secão: Triângulo, Cima ,Cima, Esquerda, Direita, Quadrado, Círculo, Direita </Text>
+         
+         <Text style={estilo.texto}>  Respeito máximo: L1, R1, Triângulo, Baixo, R2, X, L1, Cima, L2, L2, L1, L1 </Text>
+         <Text style={estilo.texto}>  Sex appeal máximo: Círculo, Triângulo, Triângulo, Cima, Círculo, R1, L2, Cima, Triângulo, L1, L1, L1 </Text>
+         <Text style={estilo.texto}>  Céu da manhã: R2, X, L1, L1, L2, L2, L2, Quadrado </Text>
+         <Text style={estilo.texto}>  Nunca ficar com fome: Quadrado, L2, R1, Triângulo, Cima, Quadrado, L2, Cima, X </Text>
+         <Text style={estilo.texto}>  Céu da noite: R2, X, L1, L1, L2, L2, L2, Triângulo </Text>
+         <Text style={estilo.texto}>  Guerra de gangues: L2, Cima, R1, R1, Esquerda, R1, R1, R2, Direita, Baixo </Text>
+         <Text style={estilo.texto}>  Sem trânsito ou NPC: X, Baixo, Cima, R2, Baixo, Triângulo, L1, Triângulo, Esquerda </Text>
+         <Text style={estilo.texto}>  Céu do meio-dia: R2, X, L1, L1, L2, L2, L2, Baixo </Text>
+         <Text style={estilo.texto}>  Céu do pôr-do-sol: Esquerda, Esquerda, L2, R1, Direita, Quadrado, Quadrado, L1, L2, X </Text>
+         <Text style={estilo.texto}>  Tempo nublado: R2, X, L1, L1, L2, L2, L2, Quadrado </Text>
+         <Text style={estilo.texto}>  Ataque de pedestres: Baixo, Cima, Cima, Cima, X, R2, R1, L2, L2 Nota: Este código não pode ser desligado. </Text>
+         <Text style={estilo.texto}>  Motim de pedestres: Baixo, Esquerda, Cima, Esquerda, X, R2, R1, L2, L1 Nota: Este código não pode ser desligado. </Text>
+         <Text style={estilo.texto}>  Pedestres com armas: R2, R1, X, Triângulo, X, Triângulo, Cima, Baixo </Text>
+         <Text style={estilo.texto}>  Pedestres atacam com armas: X, L1, Cima, Quadrado, Baixo, X, L2, Triângulo, Baixo, R1, L1, L1 </Text>
+         <Text style={estilo.texto}>  Pedestres ninjas: X, X, Baixo, R2, L2, Círculo, R1, Círculo, Quadrado </Text>
+         <Text style={estilo.texto}>  Trânsito rosa: Círculo, L1, Baixo, L2, Esquerda, X, R1, L1, Direita, Círculo </Text>
+         <Text style={estilo.texto}>  As prostitutas pagam : Direita, L2, L2, Baixo, L2, Cima, Cima, L2, R2 </Text>
+         <Text style={estilo.texto}>  Subir nível de procurado: R1, R1, Círculo, R2, Direita, Esquerda, Direita, Esquerda, Direita, Esquerda </Text>
+         <Text style={estilo.texto}>  Recrutar qualquer pessoa (com Bazuca): R2, R2, R2, X, L2, L1, R2, L1, Baixo, X </Text>
+         <Text style={estilo.texto}>  Tempestade de areia: Cima, Baixo, L1, L1, L2, L2, L1, L2, R1, R2</Text>
+         <Text style={estilo.texto}>  Recrutar qualquer pessoa (com pistola): Baixo, Quadrado, Cima, R2, R2, Cima, Direita, Direita, Cima </Text>
+         <Text style={estilo.texto}>  Jogo lento: Triângulo, Cima, Direita, Baixo, Quadrado, R2, R1</Text>
+         <Text style={estilo.texto}>  Caminhão monstro: Direita, Cima, R1, R1, R1, Baixo, Triângulo, Triângulo, X, Círculo, L1, L1 </Text>
+         <Text style={estilo.texto}>  paraquedas: Esquerda, Direita, L1, L2, R1, R2, R2, Cima, Baixo, Direita, L1</Text>
+         <Text style={estilo.texto}>  Tanque de guerra: Círculo, Círculo, L1, Círculo, Círculo, Círculo, L1, L2, R1, Triângulo, Círculo, Triângulo </Text>
+         <Text style={estilo.texto}>  limousine: R2, Cima, L2, Esquerda, Esquerda, R1, L1, Círculo, Direita </Text>
+         <Text style={estilo.texto}>  Bloodring Banger: Baixo, R1, Círculo, L2, L2, X, R1, L1, Esquerda, Esquerda </Text>
+         <Text style={estilo.texto}>  Carrinho de Golf: Círculo, L1, Cima, R1, L2, X, R1, L1, Círculo, X </Text>
+         <Text style={estilo.texto}>  Trator: R2, L1, L1, Direita, Direita, Cima, Cima, X, L1, Esquerda </Text>
+         <Text style={estilo.texto}>  Carros mais rápidos: Direita, R1, Cima, L2, L2, Esquerda, R1, L1, R1, R1 </Text>
+         <Text style={estilo.texto}>  Hotring Racer #1: R1, Círculo, R2, Direita, L1, L2, X (2), Quadrado, R1 </Text>
+         <Text style={estilo.texto}>  Hotring Racer #2: R2, L1, Círculo, Direita, L1, R1, Direita, Cima, Círculo, R2 </Text>
+         <Text style={estilo.texto}>  Helicóptero: Círculo, X, L1, Círculo, Círculo, L1, Círculo, R1, R2, L2, L1, L1 </Text>
+         <Text style={estilo.texto}>  Caminhão: R1, Cima, Esquerda, Direita, R2, Cima, Direita, Quadrado, Direita, L2, L1, L1 </Text>
+         <Text style={estilo.texto}>  Hydra: Triângulo, Triângulo, Quadrado, Círculo, X, L1, L1, Baixo, Cima </Text>
+         <Text style={estilo.texto}>  Quad Bike: Esquerda, Esquerda, Baixo, Baixo, Cima, Cima, Quadrado, Círculo, Triângulo, R1, R2 </Text>
+         <Text style={estilo.texto}>  Vortex: Triângulo, Triângulo, Quadrado, Círculo, X, L1, L2, Baixo, Baixo</Text>
+         <Text style={estilo.texto}>  Tempestade: R2, X, L1, L1, L2, L2, L2, Círculo </Text>                             
+         <Text style={estilo.texto}>  Stunt Plane: Círculo, Cima, L1, L2, Baixo, R1, L1, L1, Esquerda, Esquerda, X, Triângulo </Text>
+         <Text style={estilo.texto}>  Super Murro: Cima, Esquerda, X, Triângulo, R1, Círculo, Círculo, Círculo, L2 </Text>
+         <Text style={estilo.texto}>  Trânsito com veículos country: Triângulo, Esquerda, Quadrado, R2, Cima, L2, Baixo, L1, X, L1, L1, L1 </Text>
+         <Text style={estilo.texto}>  Todos os veículos country: L1 L1 R1 R1 L2 L1 R2 Baixo Esquerda Cima </Text>
+         <Text style={estilo.texto}>  Desbloquear Romero: Baixo, R2, Baixo, R1, L2, Esquerda, R1, L1, Esquerda, Direita </Text>
+         <Text style={estilo.texto}>  Desbloquear Trashmaster: Círculo, R1, Círculo, R1, Esquerda, Esquerda, R1, L1, Círculo, Direita </Text>
+         <Text style={estilo.texto}>  Pack 1 de armas: R1, R2, L1, R2, Esquerda, Baixo, Direita, Cima, Esquerda, Baixo, Direita, Cima </Text>
+         <Text style={estilo.texto}>  Pack 2 de armas: R1, R2, L1, R2, Esquerda, Baixo, Direita, Cima, Esquerda, Baixo, Baixo, Esquerda </Text>
+         <Text style={estilo.texto}>  Pack 3 de armas: R1, R2, L1, R2, Esquerda, Baixo, Direita, Cima, Esquerda , Baixo, Baixo, Baixo </Text>
+         <Text style={estilo.texto}>  Tempo com nuvens: L2, Baixo, Baixo, Esquerda, Quadrado, Esquerda, R2, Quadrado, X, R1, L1, L1 </Text>
+</View>
+          
+      </View>
+
+        <View style={estilo.container}>
+            <Text style={estilo.titulo} >MAPA DE PICHAÇÕES</Text>
+            <Image source={require('../../../assets/fotos/01.jpg')}
+                    style={estilo.maps}/>
+                    <Text style={estilo.texto}>RECOMPENÇAS</Text>
+                    <Text style={estilo.texto}>Molotov Cocktail, AK-47, Sawed-Off Shotgun e TEC 9 são entregues na cozinha da Casa dos Johnson.</Text>
+                    <Text style={estilo.texto}>Membros da Grove Street Families vão começar a carregar Desert Eagles, SMGs e facas ao invés da pistola padrão e TEC 9.</Text>
+            
+        </View>
+
+        <View style={estilo.container}>
+            <Text style={estilo.titulo} >MAPA DE FOTOGRAFIAS</Text>
+            <Image source={require('../../../assets/fotos/02.jpg')}
+                    style={estilo.maps2}/>
+                    <Text style={estilo.texto}>RECOMPENÇAS</Text>
+                    <Text style={estilo.texto}>Snapshots são um dos quatro itens colecionáveis, juntamente com as Tags, Horseshoes e Oysters, e, como os demais, completar todos os 50 concede ao jogador uma recompensa significativa. A recompensa por cada Snapshot obtida corretamente é de $100, perfazendo um total de $5,000 pela coleta de todos os Snapshots. A recompensa por capturar todos os 50 é também a criação de quatro tipos de armas disponíveis na Doherty Garage;: a Micro Uzi, Granadas, Pump Action Shotgun, e o Sniper Rifle, e o jogador receberá um bônus de $100,000.</Text>
+                    
+            
+        </View>
+
+        <View style={estilo.container}>
+            <Text style={estilo.titulo} >FERRADURAS</Text>
+            <Image source={require('../../../assets/fotos/03.jpg')}
+                    style={estilo.maps3}/>
+                    <Text style={estilo.texto}>RECOMPENÇAS</Text>
+                    <Text style={estilo.texto}>A coleta de todas as 50 ferraduras maximizará a sorte do jogador para 1.000 (não aumenta a chance de ganhar como mencionado anteriormente), além de premiar o jogador com $100,000. Além disso, quatro armas aparecem em frente ao Four Dragons Casino, no extremo sul de The Strip: uma MP5, Satchel Charges, uma SPAS 12 e uma M4. A coleta de todas as 50 ferraduras é necessária para 100% de conclusão.</Text>
+                    
+            
+        </View>
+
+        <View style={estilo.container}>
+            <Text style={estilo.titulo} >OSTRAS</Text>
+            <Image source={require('../../../assets/fotos/04.jpg')}
+                    style={estilo.maps4}/>
+                    <Text style={estilo.texto}>RECOMPENÇAS</Text>
+                    <Text style={estilo.texto}>A coleta de todas as 50 ostras concede ao jogador uma recompensa de $100,000, capacidade pulmonar máxima e um privilégio em que o jogador pode começar a namorar novas namoradas, independentemente da aparência ou apelo sexual do personagem.</Text>
+                    
+            
+        </View>
+
+
+            <View style={estilo.container}>
+                <Text style={estilo.contato}>contato.porfirios@gmail.com</Text>
+
+            </View>
+
+      </ScrollView>
+    );
+  } 
+          
+      
+      
+  export default Home
